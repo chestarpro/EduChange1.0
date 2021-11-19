ab19bfcdabc75167e6870fd5fa22c847a627b87b
chestarpro <chestarpro@gmail.com>
2021-11-19 20:37:59 +0600

    test permit all 3

--- src/main/java/kg/itacademy/config/SecurityConfig.java
+++ src/main/java/kg/itacademy/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/admin/*").hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/sign-up").permitAll()
                 .antMatchers(HttpMethod.POST, "/sign-in").permitAll()
-                .antMatchers("/api/*").permitAll()
+                .antMatchers("/api/*").authenticated()
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic();
